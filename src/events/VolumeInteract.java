654fac9b2d8e3928e4c21fa183640ff382c6476e
Eivind Vegsundvaag <eivindveg@hotmail.com>
2013-09-09 16:33:05 +0200

    Moved function from KeyboardListener.java to VolumeInteract.java

--- src/events/VolumeInteract.java
+++ src/events/VolumeInteract.java
@@ -4,7 +4,22 @@ public class VolumeInteract
 {
 	public static void changeVolume()
 	{
-		
+		public static void volumeInteractEvent(int slider[sliderID], int volUpDown)
+		{
+			
+			int newVolume = SoundManagerGUI.slider[sliderID].getValue() + volUpDown;
+			if (newVolume >= 0 && newVolume <= 101)
+			{
+				if(newVolume >= 100)
+				{
+					SoundManagerGUI.slider[sliderID].setValue(100);
+				}
+				else
+				{
+					SoundManagerGUI.slider[sliderID].setValue(newVolume);
+				}
+			}
+		}
 	}
 	public static void getNewVolume() //This method will be used to update the GUI
 	{
